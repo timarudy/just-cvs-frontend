4909daf1617af464dcae6d207a5ebe7b528b7486
hkravtsov04 <kravtsov420@gmail.com>
2024-11-20 15:13:54 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import './just-cvs-frontend/src/css/components/Header.css';
 
 const Header: React.FC = () => {
     return (
